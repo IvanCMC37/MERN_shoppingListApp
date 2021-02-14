045391a09b23cb4704fa1409db000d9fae48ed29
Ivan Chan <iot3534199@gmail.com>
2021-02-14 12:50:50 +1100

    Finished Redux setup

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,5 +1,10 @@
 import { v4 as uuidv4 } from "uuid";
-import { GET_ITEMS, ADD_ITEMS, DELETE_ITEMS } from "../actions/types";
+import {
+  GET_ITEMS,
+  ADD_ITEM,
+  DELETE_ITEM,
+  DELETE_ITEMS,
+} from "../actions/types";
 
 const initialState = {
   items: [
@@ -14,6 +19,16 @@ export default function (state = initialState, action) {
   switch (action.type) {
     case GET_ITEMS:
       return { ...state };
+    case DELETE_ITEM:
+      return {
+        ...state,
+        items: state.items.filter((item) => item.id !== action.payload),
+      };
+    case ADD_ITEM:
+      return {
+        ...state,
+        items: [action.payload, ...state.items],
+      };
     default:
       return state;
   }
