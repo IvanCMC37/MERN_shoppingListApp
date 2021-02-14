7aea12d4b115f8f04ff8e83f46de54a8b408d2e2
Ivan Chan <iot3534199@gmail.com>
2021-02-14 16:20:22 +1100

    Setup Heroku Config Vars

--- config/key.js
+++ config/key.js
@@ -1,4 +1,11 @@
+require("dotenv").config();
+
+const DBUserName = process.env.DBUserName;
+const DBPassword = process.env.DBPassword;
+const cluster = process.env.cluster;
+const DBName = process.env.DBName;
+
 //make sure you hide this one on source control
 module.exports = {
-  mongoURI: `mongodb+srv://${process.env.userName}:${process.env.password}@cluster0.sbto5.mongodb.net/${DBName}?retryWrites=true&w=majority`,
+  mongoURI: `mongodb+srv://${DBUserName}:${DBPassword}@${cluster}/${DBName}?retryWrites=true&w=majority`,
 };
