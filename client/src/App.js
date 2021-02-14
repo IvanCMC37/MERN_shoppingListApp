7503f5bca6e62857dd70c0cf52c81022d224d799
Ivan Chan <iot3534199@gmail.com>
2021-02-14 12:50:50 +1100

    Finished Redux setup

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import AppNavbar from "./components/AppNavbar";
 import ShoppingList from "./components/ShoppingList";
+import ItemModal from "./components/itemModal";
+import { Container } from "reactstrap";
+
 import { Provider } from "react-redux";
 import store from "./store";
 
@@ -10,7 +13,10 @@ function App() {
     <Provider store={store}>
       <div className="App">
         <AppNavbar />
-        <ShoppingList />
+        <Container>
+          <ItemModal />
+          <ShoppingList />
+        </Container>
       </div>
     </Provider>
   );
